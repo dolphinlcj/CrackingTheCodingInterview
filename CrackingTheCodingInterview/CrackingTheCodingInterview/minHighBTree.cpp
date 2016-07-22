d77bffcd67bea5f6c821936d2fd321b4522229a7
tun hai <lcj10030113@hotmail.com>
2016-07-22 19:35:42 +0800

    modify minBTree update

--- CrackingTheCodingInterview/CrackingTheCodingInterview/minHighBTree.cpp
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/minHighBTree.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <cstring>
-#define maxn 6
+#define maxn 9
 
 using namespace std;
 
@@ -15,19 +15,48 @@ TreeNode *bst, *p;
 
 void init()
 {
-	//memset(TreeNode, '\0', sizeof(TreeNode));
+	p = NULL;
+	bst = NULL;
 }
+
+void newNode(TreeNode* &vNode, int data)
+{
+	vNode = new TreeNode;
+	vNode->data = data;
+	vNode->left = NULL;
+	vNode->right = NULL;
+}
+
 void createMinHighBTree(TreeNode * &head, int a[], int left, int right)
 {
-	if (left <= right)
+	/*
+	if (head == NULL)
 	{
-		int mid = left + (left + right) >> 1;
+		mid = (left + right) >> 1;
 		p = new TreeNode();
 		p->data = a[mid];
-
-		createMinHighBTree(p->left, a, left, mid -1);
-		createMinHighBTree(p->right, a, mid+1, right);
+		head = p;
 	}
+	else
+	{
+		if (left <= right)
+		{
+			createMinHighBTree(head->left, a, left, mid - 1);
+			createMinHighBTree(head->right, a, mid + 1, right);
+		}
+			
+	}
+    */
+
+	if (left <= right)
+	{
+		int mid = (left + right) >> 1;
+		newNode(head, a[mid]);
+
+		createMinHighBTree(head->left, a, left, mid-1);
+		createMinHighBTree(head->right, a, mid + 1, right);
+	}
+	
 }
 
 int getHight(TreeNode *head)
@@ -42,17 +71,19 @@ int getHight(TreeNode *head)
 		int rightHight = getHight(head->right);
 		return (leftHight > rightHight) ? leftHight + 1 : rightHight + 1;
 	}
+	//second method
+	//if (head == NULL) return 0;
 	//return max(getHight(head->left), getHight(head->right)) + 1;
 }
 
 int main()
 {
 	init();
-	TreeNode *head;
-	int a[maxn] = {1, 3, 5, 8, 9, 11};
+	int a[maxn] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
 	
-	createMinHighBTree(head, a, 0, maxn);
-	cout << getHight(head) << endl;
+	createMinHighBTree(bst, a, 0, maxn-1);
+	cout << getHight(bst) << endl;
+	
 	system("pause");
 	return 0;
 }
