6701626af633e14009b74ad217d259901487a3de
tun hai <lcj10030113@hotmail.com>
2016-07-31 20:47:14 +0800

    find the first ancestor update

--- CrackingTheCodingInterview/CrackingTheCodingInterview/findFirstAncestor.cpp
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/findFirstAncestor.cpp
@@ -55,10 +55,77 @@ Node* findFirstAncestor(Node* n1, Node* n2)
 	}
 	return n2;
 }
+bool father(Node* n1, Node* n2)
+{
+	if (n1 == NULL)
+	{
+		return false;
+	}
+	else if (n1 == n2)
+	{
+		return true;
+	}
+	else
+	{
+		return father(n1->left, n2) || father(n1->right, n2);
+	}
+}
+
+Node* findFirstAncestor1(Node* n1, Node* n2)
+{
+	if (n1 == NULL || n2 == NULL)
+	{
+		return NULL;
+	}
+
+	while (n1)
+	{
+		if (father(n1, n2))
+		{
+			return n1;
+		}
+		n1 = n1->parent;
+
+	}
+
+	return NULL;
+}
+
+//without parent pointer
+Node* findFirstAncestor2(Node* head, Node* n1, Node* n2, Node* finalNode)
+{
+	if (head == NULL || n1 == NULL || n2 == NULL)
+	{
+		return NULL;
+	}
+	//find the last same ancestor, the view is great
+	if (head && father(head, n1) && father(head, n2))
+	{
+		finalNode = head;
+		findFirstAncestor2(head->left, n1, n2, finalNode);
+		findFirstAncestor2(head->right, n1, n2, finalNode);
+	}
+
+}
 
 Node* search(Node* head, int x)
 {
-	return NULL;
+	if (head == NULL)
+	{
+		return NULL;
+	}
+	else if (x == head->data)
+	{
+		return head;
+	}
+	else if (x <= head->data)
+	{
+		search(head->left, x);
+	}
+	else
+	{
+		search(head->right, x);
+	}
 }
 int main()
 {
@@ -75,7 +142,14 @@ int main()
 	
 	Node * n = findFirstAncestor(n1, n2);
 	cout << n->data << endl;
+
+	Node* m1 = findFirstAncestor1(n1, n2);
+	cout << m1->data << endl;
 	
+	Node* finalNode = NULL;
+	Node* m2 = findFirstAncestor2(head, n1, n2, finalNode);
+	cout << m2->data << endl;
+
 	system("pause");
 	return 0;
 }
